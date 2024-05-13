66962d3bdd157d6aae30e4c8ea023c3e9d8d03b0
Richard Darst <rkd@zgib.net>
2024-05-13 22:28:02 +0300

    make-zip-app: make work with other venv locations

--- make-zip-app.sh
+++ make-zip-app.sh
@@ -1,8 +1,11 @@
+set -e
+
+BASE=$(python3 -c 'import obsws_python; from os.path import dirname; print(dirname(dirname(obsws_python.__file__)))')
+
 rm -r zipapp
 mkdir zipapp
-rsync -a venv/lib/python3.11/site-packages/obsws_python/ zipapp/obsws_python/
-rsync -a venv/lib/python3.11/site-packages/tktooltip/ zipapp/tktooltip/
+rsync -a ${BASE}/obsws_python/ zipapp/obsws_python/
+rsync -a ${BASE}/tktooltip/ zipapp/tktooltip/
 rsync -a obs_cr/ zipapp/obs_cr/
-
 
 python -m zipapp zipapp --main=obs_cr.__main__:main --python="/usr/bin/env python3" --output obs-cr.pyz
