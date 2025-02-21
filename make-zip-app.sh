0b24e817a8cdde1f49ce1accd00adfe322547448
Richard Darst <rkd@zgib.net>
2025-02-21 13:15:26 +0200

    action: don't build zipapp anymore

--- make-zip-app.sh
+++ make-zip-app.sh
@@ -4,10 +4,10 @@ BASE=$(python3 -c 'import obsws_python; from os.path import dirname; print(dirna
 
 rm -rf zipapp
 mkdir zipapp
-rsync -a -L ${BASE}/obsws_python*                   zipapp/
-rsync -a -L ${BASE}/websocket*                      zipapp/
-rsync -a -L ${BASE}/tktooltip                       zipapp/
-rsync -a -L ${BASE}/tkinter_tooltip-3.1.0.dist-info zipapp/
-rsync -a -L obs_cr                                  zipapp/
+rsync -a ${BASE}/obsws_python*                   zipapp/
+rsync -a ${BASE}/websocket*                      zipapp/
+rsync -a ${BASE}/tktooltip                       zipapp/
+rsync -a ${BASE}/tkinter_tooltip-3.1.0.dist-info zipapp/
+rsync -a obs_cr                                  zipapp/
 
 python -m zipapp zipapp --main=obs_cr.__main__:main --python="/usr/bin/env python3" --output obs-cr.pyz
